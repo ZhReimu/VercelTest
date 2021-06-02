212fbb7b82e8698aa650d0424690a7d3e822bce5
Mr.X <58155247+ZhReimu@users.noreply.github.com>
2021-06-02 23:17:42 +0800

    尝试修复 bug

--- api/api.py
+++ api/api.py
@@ -13,6 +13,6 @@ class handler(BaseHTTPRequestHandler):
                 ip = request.args['ip']
             except KeyError:
                 ip = request.remote_addr
-            return json.loads(
+            return json.loads(json.dumps(
                 "{'code': '200',"f"'db_info': f'{cz_ip.get_version()} 当前一共有 {cz_ip.index_count} 条记录'," "'data': {'ip': "f"{ip}," f"'city': {cz_ip.get_addr_by_ip(ip)}" "} "
-            )
+            ))
