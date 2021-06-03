78cdb826853e93eeb8574f20dcd2b7279fea7bc3
博麗靈夢 <1725338233@qq.com>
2021-06-03 21:46:04 +0800

    继续尝试排查 获取不到 客户端 IP 的问题

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-import requests
+import urllib.request
 
 from api.ip_data import CzIp
 
@@ -19,7 +19,7 @@ class handler(BaseHTTPRequestHandler):
         else:
             ip = self.client_address[0]
         print(self.address_string())
-        print(requests.get('http://httpbin.org/get').text)
+        print(urllib.request.urlopen("https://httpbin.org/get").read().decode("utf-8"))
         code = 200
         db_info = f'{cz_ip.get_version()} 当前一共有 {cz_ip.index_count} 条记录'
         data = str({"ip": ip, "city": cz_ip.get_addr_by_ip(ip)}).replace('\'', '"')
